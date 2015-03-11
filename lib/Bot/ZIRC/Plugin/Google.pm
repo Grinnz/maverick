919e831cff21f4d64783fbc2a8bd9055db47d07e
Dan Book <grinnz@grinnz.com>
2015-03-10 23:52:47 -0400

    google image search

--- lib/Bot/ZIRC/Plugin/Google.pm
+++ lib/Bot/ZIRC/Plugin/Google.pm
@@ -57,26 +57,75 @@ sub register {
 				
 				my $first_result = shift @$results;
 				my $channel_name = lc ($channel // $sender);
-				$self->results_cache->{$network}{$channel_name} = $results;
+				$self->results_cache->{web}{$network}{$channel_name} = $results;
 				my $show_more = @$results;
-				display_result($network, $sender, $channel, $first_result, $show_more);
+				display_result_web($network, $sender, $channel, $first_result, $show_more);
 			});
 		},
 		on_more => sub {
 			my ($network, $sender, $channel) = @_;
 			my $channel_name = lc ($channel // $sender);
-			my $results = $self->results_cache->{$network}{$channel_name};
+			my $results = $self->results_cache->{web}{$network}{$channel_name};
 			return $network->reply($sender, $channel, "No more results for Google search")
 				unless $results and @$results;
 			
 			my $next_result = shift @$results;
 			my $show_more = @$results;
-			display_result($network, $sender, $channel, $next_result, $show_more);
+			display_result_web($network, $sender, $channel, $next_result, $show_more);
+		},
+	);
+	
+	$bot->add_command(
+		name => 'image',
+		help_text => 'Search for images with Google',
+		usage_text => '<query>',
+		tokenize => 0,
+		on_run => sub {
+			my ($network, $sender, $channel, $query) = @_;
+			return 'usage' unless length $query;
+			my $api_key = $network->config->get('apis','google_api_key');
+			my $cse_id = $network->config->get('apis','google_cse_id');
+			die GOOGLE_API_KEY_MISSING unless defined $api_key and defined $cse_id;
+			
+			my $request = Mojo::URL->new(GOOGLE_API_ENDPOINT)
+				->query(key => $api_key, cx => $cse_id, q => $query,
+				safe => 'high', searchType => 'image');
+			
+			$self->ua->get($request, sub {
+				my ($ua, $tx) = @_;
+				if (my $err = $tx->error) {
+					my $msg = $err->{code}
+						? "$err->{code} response: $err->{message}"
+						: "Connection error: $err->{message}";
+					return $network->reply($sender, $channel,
+						"Error retrieving Google image search results: $msg");
+				}
+				my $results = $tx->res->json->{items};
+				return $network->reply($sender, $channel, "No results for Google image search")
+					unless $results and @$results;
+				
+				my $first_result = shift @$results;
+				my $channel_name = lc ($channel // $sender);
+				$self->results_cache->{image}{$network}{$channel_name} = $results;
+				my $show_more = @$results;
+				display_result_image($network, $sender, $channel, $first_result, $show_more);
+			});
+		},
+		on_more => sub {
+			my ($network, $sender, $channel) = @_;
+			my $channel_name = lc ($channel // $sender);
+			my $results = $self->results_cache->{image}{$network}{$channel_name};
+			return $network->reply($sender, $channel, "No more results for Google image search")
+				unless $results and @$results;
+			
+			my $next_result = shift @$results;
+			my $show_more = @$results;
+			display_result_image($network, $sender, $channel, $next_result, $show_more);
 		},
 	);
 }
 
-sub display_result {
+sub display_result_web {
 	my ($network, $sender, $channel, $result, $show_more) = @_;
 	my $url = $result->{link} // '';
 	my $title = $result->{title} // '';
@@ -89,7 +138,19 @@ sub display_result {
 	my $b_code = chr 2;
 	my $response = "Google search result: $b_code$title$b_code - " .
 		"$url$snippet$if_show_more";
-	warn $response;
+	$network->reply($sender, $channel, $response);
+}
+
+sub display_result_image {
+	my ($network, $sender, $channel, $result, $show_more) = @_;
+	my $url = $result->{link} // '';
+	my $title = $result->{title} // '';
+	my $context_url = $result->{image}{contextLink} // '';
+	my $if_show_more = $show_more ? " [ $show_more more results, use 'more' command to display ]" : '';
+	
+	my $b_code = chr 2;
+	my $response = "Image search result: $b_code$title$b_code - " .
+		"$url ($context_url)$if_show_more";
 	$network->reply($sender, $channel, $response);
 }
 
