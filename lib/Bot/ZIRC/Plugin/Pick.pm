6b5a33337127abb0b34a32558235237019823717
Dan Book <grinnz@grinnz.com>
2015-03-13 01:39:58 -0400

    pick plugin

---
+++ lib/Bot/ZIRC/Plugin/Pick.pm
@@ -0,0 +1,27 @@
+package Bot::ZIRC::Plugin::Pick;
+
+use Moo;
+use namespace::clean;
+
+with 'Bot::ZIRC::Plugin';
+
+sub register {
+	my ($self, $bot) = @_;
+	
+	$bot->add_command(
+		name => 'pick',
+		help_text => 'Pick a random item from a list',
+		usage_text => '<item>[, <item>...]',
+		tokenize => 0,
+		on_run => sub {
+			my ($network, $sender, $channel, $items) = @_;
+			return 'usage' unless length $items;
+			my @items = split /\s*,\s*/, $items;
+			my $choice = 1+int rand @items;
+			my $item = $items[$choice-1];
+			$network->reply($sender, $channel, "$choice: $item");
+		},
+	);
+}
+
+1;
