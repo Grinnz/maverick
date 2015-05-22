afa4430dcd629a57a927807f8dd668636d675865
Dan Book <grinnz@grinnz.com>
2015-05-21 23:36:07 -0400

    add NoAuth network subclass

---
+++ lib/Bot/Maverick/Network/NoAuth.pm
@@ -0,0 +1,20 @@
+package Bot::Maverick::Network::NoAuth;
+
+use Moo;
+use namespace::clean;
+
+extends 'Bot::Maverick::Network';
+
+our $VERSION = '0.20';
+
+sub identify {}
+
+around 'user' => sub {
+	my $orig = shift;
+	my $user = $orig->(@_);
+	$user->is_registered(1);
+	$user->identity($user->nick);
+	return $user;
+};
+
+1;
