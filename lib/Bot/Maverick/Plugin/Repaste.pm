b035362b5f790d916bc141b876f05759309497f1
Dan Book <grinnz@grinnz.com>
2015-09-03 22:56:09 -0400

    split up repasting plugin into sub-delays, handle weird responses from pastebin.com

--- lib/Bot/Maverick/Plugin/Repaste.pm
+++ lib/Bot/Maverick/Plugin/Repaste.pm
@@ -21,66 +21,133 @@ sub register {
 		
 		my @pastebin_keys = ($m->text =~ m!\bpastebin\.com/(?:raw\.php\?i=)?([a-z0-9]+)!ig);
 		my @hastebin_keys = ($m->text =~ m!\bhastebin\.com/(?:raw/)?([a-z]+)!ig);
-		return unless @pastebin_keys or @hastebin_keys;
+		my @pastes = (map { +{type => 'pastebin', key => $_} } @pastebin_keys),
+			(map { +{type => 'hastebin', key => $_} } @hastebin_keys);
+		return undef unless @pastes;
 		
 		Mojo::IOLoop->delay(sub {
 			my $delay = shift;
-			foreach my $paste_key (@pastebin_keys) {
-				my $url = Mojo::URL->new(PASTEBIN_RAW_ENDPOINT)->query(i => $paste_key);
-				$m->logger->debug("Found pastebin link to $paste_key: $url");
-				$bot->ua->get($url, $delay->begin);
-			}
-			foreach my $paste_key (@hastebin_keys) {
-				my $url = Mojo::URL->new(HASTEBIN_RAW_ENDPOINT)->path($paste_key);
-				$m->logger->debug("Found hastebin link to $paste_key: $url");
-				$bot->ua->get($url, $delay->begin);
-			}
+			$self->_retrieve_pastes($m, \@pastes, $delay->begin(0))
+				->catch(sub { $delay->emit(error => $_[1]) });
 		}, sub {
-			my $delay = shift;
-			foreach my $tx (@_) {
-				$m->logger->error($self->ua_error($tx->error)), next if $tx->error;
-				my $contents = $tx->res->text;
-				$m->logger->debug("No paste contents"), next unless length $contents;
-				
-				my $lang = $m->config->channel_param($m->channel, 'repaste_lang') // 'text';
-				
-				my %form = (
-					paste_data => $contents,
-					paste_lang => $lang,
-					paste_user => $m->sender->nick,
-					paste_private => 'yes',
-					paste_expire => 86400,
-					api_submit => 'true',
-					mode => 'json',
-				);
-				
-				$m->logger->debug("Repasting contents to fpaste");
-				$bot->ua->post(FPASTE_PASTE_ENDPOINT, form => \%form, $delay->begin);
-			}
+			my ($delay, $pastes) = @_;
+			$self->_repaste_pastes($m, $pastes, $delay->begin(0))
+				->catch(sub { $delay->emit(error => $_[1]) });
 		}, sub {
-			my $delay = shift;
-			my @urls;
-			foreach my $tx (@_) {
-				$m->logger->error($self->ua_error($tx->error)), next if $tx->error;
-				
-				my $result = $tx->res->json->{result} // {};
-				$m->logger->error("Paste error: ".$result->{error}), next if $result->{error};
-				
-				my $id = $result->{id};
-				my $hash = $result->{hash} // '';
-				$m->logger->error("No paste ID returned"), next unless defined $id;
-				
-				my $url = Mojo::URL->new(FPASTE_PASTE_ENDPOINT)->path("$id/$hash/");
-				$m->logger->debug("Repasted to $url");
-				push @urls, $url;
-			}
-			
-			return undef unless @urls;
+			my ($delay, $urls) = @_;
+			return undef unless @$urls;
 			my $reply = 'Repasted text';
 			$reply .= ' from ' . $m->sender if defined $m->channel;
-			$reply .= ': ' . join ' ', @urls;
+			$reply .= ': ' . join ' ', @$urls;
 			$m->reply_bare($reply);
-		})->catch(sub { chomp (my $err = $_[1]); $m->logger->error("Error repasting pastebin @pastebin_keys @hastebin_keys: $err") });
+		})->catch(sub { chomp (my $err = $_[1]); $m->logger->error("Error repasting pastes from message '" . $m->text . "': $err") });
+	});
+}
+
+sub _retrieve_pastes {
+	my $cb = pop;
+	my ($self, $m, $pastes) = @_;
+	
+	return Mojo::IOLoop->delay(sub {
+		my $delay = shift;
+		foreach my $paste (@$pastes) {
+			my $url;
+			if ($paste->{type} eq 'pastebin') {
+				$url = Mojo::URL->new(PASTEBIN_RAW_ENDPOINT)->query(i => $paste->{key});
+			} elsif ($paste->{type} eq 'hastebin') {
+				$url = Mojo::URL->new(HASTEBIN_RAW_ENDPOINT)->path($paste->{key});
+			}
+			$m->logger->debug("Found $paste->{type} link to $paste->{key}: $url");
+			$self->ua->get($url, $delay->begin);
+		}
+	}, sub {
+		my ($delay, @txs) = @_;
+		my $recheck_pastebin;
+		foreach my $i (0..$#txs) {
+			my $tx = $txs[$i];
+			my $paste = $pastes->[$i];
+			$m->logger->error($self->ua_error($tx->error)), next if $tx->error;
+			my $contents = $tx->res->text;
+			$m->logger->debug("No paste contents for $paste->{type} paste $paste->{key}"), next unless length $contents;
+			if ($paste->{type} eq 'pastebin' and $contents =~ /Please refresh the page to continue\.\.\./) {
+				$paste->{recheck} = $recheck_pastebin = 1;
+			} else {
+				$paste->{contents} = $contents;
+			}
+		}
+		
+		if ($recheck_pastebin) {
+			foreach my $paste (@$pastes) {
+				$delay->pass(undef), next unless $paste->{recheck};
+				my $url = Mojo::URL->new(PASTEBIN_RAW_ENDPOINT)->query(i => $paste->{key});
+				$m->logger->debug("Rechecking $paste->{type} paste $paste->{key}: $url");
+				my $end = $delay->begin;
+				Mojo::IOLoop->timer(1 => sub { $self->ua->get($url, $end) });
+			}
+		} else {
+			$cb->($pastes);
+		}
+	}, sub {
+		my ($delay, @txs) = @_;
+		foreach my $i (0..$#txs) {
+			my $tx = $txs[$i];
+			my $paste = $pastes->[$i];
+			next unless defined $tx;
+			$m->logger->error($self->ua_error($tx->error)), next if $tx->error;
+			my $contents = $tx->res->text;
+			$m->logger->debug("No paste contents for $paste->{type} paste $paste->{key}"), next unless length $contents;
+			$paste->{contents} = $contents;
+		}
+		$cb->($pastes);
+	});
+}
+
+sub _repaste_pastes {
+	my $cb = pop;
+	my ($self, $m, $pastes) = @_;
+	
+	return Mojo::IOLoop->delay(sub {
+		my $delay = shift;
+		foreach my $paste (@$pastes) {
+			$delay->pass(undef), next unless defined $paste->{contents};
+			
+			my $lang = $m->config->channel_param($m->channel, 'repaste_lang') // 'text';
+			
+			my %form = (
+				paste_data => $paste->{contents},
+				paste_lang => $lang,
+				paste_user => $m->sender->nick,
+				paste_private => 'yes',
+				paste_expire => 86400,
+				api_submit => 'true',
+				mode => 'json',
+			);
+			
+			$m->logger->debug("Repasting $paste->{type} paste $paste->{key} contents to fpaste");
+			$self->ua->post(FPASTE_PASTE_ENDPOINT, form => \%form, $delay->begin);
+		}
+	}, sub {
+		my ($delay, @txs) = @_;
+		my @urls;
+		foreach my $i (0..$#txs) {
+			my $tx = $txs[$i];
+			my $paste = $pastes->[$i];
+			next unless defined $tx;
+			$m->logger->error($self->ua_error($tx->error)), next if $tx->error;
+			
+			my $result = $tx->res->json->{result} // {};
+			$m->logger->error("Paste error: ".$result->{error}), next if $result->{error};
+			
+			my $id = $result->{id};
+			my $hash = $result->{hash} // '';
+			$m->logger->error("No paste ID returned"), next unless defined $id;
+			
+			my $url = Mojo::URL->new(FPASTE_PASTE_ENDPOINT)->path("$id/$hash/");
+			$m->logger->debug("Repasted $paste->{type} paste $paste->{key} to $url");
+			push @urls, $url;
+		}
+		
+		$cb->(\@urls);
 	});
 }
 
