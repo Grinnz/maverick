ba5c22ce693f09a1b8acb949419c511dc1b1c8e5
Dan Book <grinnz@grinnz.com>
2021-06-01 19:55:38 -0400

    mark users with bot cloaks as bots on freenode-like networks

--- lib/Bot/Maverick/Network/Freenode.pm
+++ lib/Bot/Maverick/Network/Freenode.pm
@@ -7,4 +7,18 @@ extends 'Bot::Maverick::Network';
 
 our $VERSION = '0.50';
 
+after '_irc_rpl_whoreply' => sub {
+	my ($self, $message) = @_;
+	my ($to, $channel, $username, $host, $server, $nick, $state, $realname) = @{$message->{params}};
+	my $user = $self->user($nick);
+	$user->is_bot(1) if $host =~ m!/bot/!;
+};
+
+after '_irc_rpl_whoisuser' => sub {
+	my ($self, $message) = @_;
+	my ($to, $nick, $username, $host, $star, $realname) = @{$message->{params}};
+	my $user = $self->user($nick);
+	$user->is_bot(1) if $host =~ m!/bot/!;
+};
+
 1;
