0a1c590d7ab0982a9bf512845861061cdc2ae756
Dan Book <grinnz@grinnz.com>
2021-06-01 20:06:03 -0400

    debug logging for bot masks

--- lib/Bot/Maverick/Network/Freenode.pm
+++ lib/Bot/Maverick/Network/Freenode.pm
@@ -10,15 +10,19 @@ our $VERSION = '0.50';
 after '_irc_rpl_whoreply' => sub {
 	my ($self, $message) = @_;
 	my ($to, $channel, $username, $host, $server, $nick, $state, $realname) = @{$message->{params}};
-	my $user = $self->user($nick);
-	$user->is_bot(1) if $host =~ m!/bot/!;
+	if ($host =~ m!/bot/!) {
+		$self->logger->debug("Received /bot/ hostmask for $nick");
+		$self->user($nick)->is_bot(1);
+	}
 };
 
 after '_irc_rpl_whoisuser' => sub {
 	my ($self, $message) = @_;
 	my ($to, $nick, $username, $host, $star, $realname) = @{$message->{params}};
-	my $user = $self->user($nick);
-	$user->is_bot(1) if $host =~ m!/bot/!;
+	if ($host =~ m!/bot/!) {
+		$self->logger->debug("Received /bot/ hostmask for $nick");
+		$self->user($nick)->is_bot(1);
+	}
 };
 
 1;
