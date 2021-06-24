50d43dd97ee437a7d76c23322c62aee046d22a5a
Dan Book <grinnz@grinnz.com>
2021-06-24 18:59:51 -0400

    allow bot at beginning or end of hostmask for libera bot detection

--- lib/Bot/Maverick/Network/Libera.pm
+++ lib/Bot/Maverick/Network/Libera.pm
@@ -12,7 +12,7 @@ our $VERSION = '0.50';
 before [qw(_irc_invite _irc_join _irc_kick _irc_mode _irc_nick _irc_notice _irc_part _irc_privmsg _irc_public _irc_quit)] => sub {
 	my ($self, $message) = @_;
 	my ($nick, undef, $host) = parse_user($message->{prefix});
-	if ($host =~ m!/bot/! and !$self->user($nick)->is_bot) {
+	if ($host =~ m!(\A|/)bot(/|\z)! and !$self->user($nick)->is_bot) {
 		$self->logger->debug("Marking $nick as a bot due to /bot/ hostmask");
 		$self->user($nick)->is_bot(1);
 	}
